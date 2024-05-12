dbf74026a2d32a05e1b5886361ed87fd316d9e2f
Pulkit Aggarwal <pulkitaggarwal251@gmail.com>
2024-05-12 16:56:35 +0530

    Authorizations

--- src/test/java/Day6/Authorization.java
+++ src/test/java/Day6/Authorization.java
@@ -10,9 +10,13 @@ package Day6;
 
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+//token - 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
 public class Authorization {
 
     @Test(priority = 1)
@@ -50,4 +54,44 @@ public class Authorization {
                 .body("authenticated",equalTo(true))
                 .log().all();
     }
+
+    @Test
+    void testBearerTokenAuthentication(){
+        String bearerToken="ghp_bbn6pV03RwqXGOnH4S5E9Apu9uVYaC0xTiY";
+
+        given()
+                .headers("Authorization","Bearer " + bearerToken)
+                .when()
+                .get("https://api.github.com/user/repos")
+                .then()
+                .statusCode(401)
+                .body("message",equalTo("Bad credentials"))
+                .log().all();
+    }
+
+    void OAuth1authentication(){
+        given()
+                .auth().oauth("consumerKey","consumerSecret","accessToken","tokenSecret")
+                .when()
+                .get("url")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void ApikeyUsage(){
+        Map<String,String> m = new HashMap<>();
+        m.put("id","524901");
+        m.put("appid","9bd223911d23eaedeecdb4a5a6a32255");
+        given()
+                .pathParam("mypath","data/2.5/forecast/")
+                .queryParams(m)
+                .when()
+                .get("http://api.openweathermap.org/{mypath}")
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
 }
